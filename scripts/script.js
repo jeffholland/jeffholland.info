5eccb6e5bc1e3d9ddfb4a0d5fba21e7244e941b1
Jeff Holland <jh6547@nyu.edu>
2022-09-16 15:33:38 -0400

    Make button disappear

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 function myFunction() {
     const header = document.getElementById("header");
-    const button = document.getElementById("myButton")
+    const button = document.getElementById("myButton");
 
     header.innerHTML = "Did you seriously just click that button?";
     button.style.visibility = "hidden";
+
+    console.log("Button clicked successfully");
 }
