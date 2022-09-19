990e915083982e36123fe50ff0376a9cd347ff78
Jeff Holland <jh6547@nyu.edu>
2022-09-19 13:42:29 -0400

    added keyboard input test to fun

--- scripts/script.js
+++ scripts/script.js
@@ -16,3 +16,32 @@ function randomNumber() {
     result.innerHTML = num.toString();
 }
 
+
+// following is pasted from Stack Overflow:
+// https://stackoverflow.com/questions/4416505/how-to-take-keyboard-input-in-javascript
+
+window.addEventListener("keydown", function (event) {
+    if (event.defaultPrevented) {
+      return; // Do nothing if the event was already processed
+    }
+  
+    switch (event.key) {
+      case "ArrowDown":
+        this.alert("You pressed down");
+        break;
+      case "ArrowUp":
+        this.alert("You pressed up");
+        break;
+      case "ArrowLeft":
+        this.alert("You pressed left");
+        break;
+      case "ArrowRight":
+        this.alert("You pressed right");
+        break;
+      default:
+        return; // Quit when this doesn't handle the key event.
+    }
+  
+    // Cancel the default action to avoid it being handled twice
+    event.preventDefault();
+  }, true);
