1fa9b826f02dcfe10d451e5a8b287dfba864d364
Jeff Holland <jh6547@nyu.edu>
2022-09-19 16:00:15 -0400

    copied code from online to animate stuff

--- scripts/snake.js
+++ scripts/snake.js
@@ -1,47 +1,90 @@
-const speed = 20;
+// const speed = 20;
 
-window.addEventListener("keydown", function (event) {
-    if (event.defaultPrevented) {
-        return;
-    }
+// window.addEventListener("keydown", function (event) {
+//     if (event.defaultPrevented) {
+//         return;
+//     }
 
-    // Get snake and its rectangle
-    const snake = document.getElementById("snake");
-    const rect = snake.getBoundingClientRect();
+//     // Get snake and its rectangle
+//     const snake = document.getElementById("snake");
+//     const rect = snake.getBoundingClientRect();
 
-    // Modify position with arrow keys
-    switch (event.key) {
-        case "ArrowDown":
-            if (rect.bottom >= this.screen.height) {
-                return;
-            }
-            snake.style.top = String(rect.top + speed) + "px";
-            break;
-        case "ArrowUp":
-            if (rect.top <= 0) {
-                return;
-            }
-            snake.style.top = String(rect.top - speed) + "px";
-            break;
-        case "ArrowLeft":
-            if (rect.left <= 0) {
-                return;
-            }
-            snake.style.left = String(rect.left - speed) + "px";
-            break;
-        case "ArrowRight":
-            if (rect.right >= this.screen.width) {
-                return;
-            }
-            snake.style.left = String(rect.left + speed) + "px";
-            break;
-    }
-}, true);
+//     // Modify position with arrow keys
+//     switch (event.key) {
+//         case "ArrowDown":
+//             if (rect.bottom >= this.screen.height) {
+//                 return;
+//             }
+//             snake.style.top = String(rect.top + speed) + "px";
+//             break;
+//         case "ArrowUp":
+//             if (rect.top <= 0) {
+//                 return;
+//             }
+//             snake.style.top = String(rect.top - speed) + "px";
+//             break;
+//         case "ArrowLeft":
+//             if (rect.left <= 0) {
+//                 return;
+//             }
+//             snake.style.left = String(rect.left - speed) + "px";
+//             break;
+//         case "ArrowRight":
+//             if (rect.right >= this.screen.width) {
+//                 return;
+//             }
+//             snake.style.left = String(rect.left + speed) + "px";
+//             break;
+//     }
+// }, true);
 
 
 // The following code is taken and modified from:
 // https://www.sitepoint.com/quick-tip-game-loop-in-javascript/
 
-function loop(timestamp) {
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var width;
+var height;
 
+var resize = function() {
+  width = window.innerWidth * 2;
+  height = window.innerHeight * 2;
+  canvas.width = width;
+  canvas.height = height;
 }
+window.onresize = resize;
+resize();
+
+ctx.fillStyle("green");
+
+var state = {
+    x: (width / 2),
+    y: (height / 2),
+}
+
+function update(progress) {
+    state.x += progress;
+    if (state.x > width) {
+        state.x -= width;
+    }
+}
+
+function draw() {
+    ctx.clearRect(0, 0, width, height);
+
+    ctx.fillRect(state.x - 10, state.y - 10, 20, 20);
+}
+
+function loop(timestamp) {
+    var progress = timestamp - lastRender;
+
+    update(progress);
+    draw();
+
+    lastRender = timestamp;
+    window.requestAnimationFrame(loop);
+}
+
+var lastRender = 0;
+window.requestAnimationFrame(loop);
