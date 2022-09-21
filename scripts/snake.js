108a1a9f9e2a95d3d0b0795addf1968d909d1af1
Jeff Holland <jh6547@nyu.edu>
2022-09-20 21:28:36 -0400

    refactored into OOP code

--- scripts/snake.js
+++ scripts/snake.js
@@ -1,85 +1,133 @@
+//==============================================
+// game canvas aka the whole dang window
+//==============================================
 
-// game canvas is the whole dang window
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var width;
 var height;
 
-// direction variable 
-// (should probably be an enum but that seemed like a hassle)
-let direction = "up";
-
-// size of one "unit" of the green snake
-const snake_unit_size = 40;
-
-// yellow dots aka snake food
-let yellow_dots = [];
-// likelihood that they will spawn is one out of...
-const yellow_dot_spawn_chance = 100;
-// size
-const yellow_dot_size = 20;
-
 var resize = function() {
-  width = window.innerWidth * 2;
-  height = window.innerHeight * 2;
-  canvas.width = width;
-  canvas.height = height;
+    width = window.innerWidth * 2;
+    height = window.innerHeight * 2;
+    canvas.width = width;
+    canvas.height = height;
 }
 window.onresize = resize;
 resize();
 
-var state = {
-    x: (width / 2),
-    y: (height / 2),
+
+
+
+
+//==============================================
+// green player aka snake
+//==============================================
+
+const Player = {
+    direction: "up",
+    size: 40,
+    state: {
+        x: (width / 2),
+        y: (height / 2)
+    },
+
+    move: function(progress) {
+            switch (this.direction) {
+                case "up":
+                    this.state.y -= progress;
+                    break;
+                case "down":
+                    this.state.y += progress;
+                    break;
+                case "left":
+                    this.state.x -= progress;
+                    break;
+                case "right":
+                    this.state.x += progress;
+                    break;
+            }
+
+            // Set wall boundaries
+
+            if (this.state.x > width) {
+                this.state.x -= width;
+            }
+            if (this.state.x < 0) {
+                this.state.x += width;
+            }
+            if (this.state.y > height) {
+                this.state.y -= height;
+            }
+            if (this.state.y < 0) {
+                this.state.y += height;
+            }
+    }
 }
+
+// // direction variable 
+// // (should probably be an enum but that seemed like a hassle)
+// let direction = "up";
+
+// // size of one "unit" of the green snake
+// const snake_unit_size = 40;
+
+// // current player state
+// var state = {
+//     x: (width / 2),
+//     y: (height / 2),
+// }
+
+
+
+
+
+//==============================================
+// yellow dots aka snake food
+//==============================================
+
+// factory
+function createYellowDot(x_coord, y_coord) {
+    return {
+        x: x_coord,
+        y: y_coord
+    }
+}
+
+// array
+let yellow_dots = [];
+
+// likelihood that they will spawn is one out of...
+const yellow_dot_spawn_chance = 100;
+
+// size
+const yellow_dot_size = 20;
+
+// spawn function
+function spawn_dots() {
+    spawn_chance = Math.floor(Math.random() * yellow_dot_spawn_chance);
+    if (spawn_chance == 0) {
+        const x_coord = Math.floor(Math.random() * width);
+        const y_coord = Math.floor(Math.random() * height);
+
+        yellow_dots.push( createYellowDot(x_coord, y_coord) );
+    }
+}
+
+
+
+
+//==============================================
+// game loop
+//==============================================
 
 function update(progress) {
 
-    // Movement
-
-    switch (direction) {
-        case "up":
-            state.y -= progress;
-            break;
-        case "down":
-            state.y += progress;
-            break;
-        case "left":
-            state.x -= progress;
-            break;
-        case "right":
-            state.x += progress;
-            break;
-    }
-
-    // Set wall boundaries
-
-    if (state.x > width) {
-        state.x -= width;
-    }
-    if (state.x < 0) {
-        state.x += width;
-    }
-    if (state.y > height) {
-        state.y -= height;
-    }
-    if (state.y < 0) {
-        state.y += height;
-    }
+    // Move player according to direction
+    Player.move(progress);
     
-
     // Spawn yellow dots randomly
-    spawn_chance = Math.floor(Math.random() * yellow_dot_spawn_chance);
-    if (spawn_chance == 0) {
-        x_coord = Math.floor(Math.random() * width);
-        y_coord = Math.floor(Math.random() * height);
-
-        let yellow_dot = {
-            x: x_coord,
-            y: y_coord
-        }
-        yellow_dots.push( yellow_dot );
-    }
+    spawn_dots();
 }
 
 function draw() {
@@ -87,7 +135,7 @@ function draw() {
 
     // Draw the green dot (snake)
     ctx.fillStyle = "green";
-    ctx.fillRect(state.x - 10, state.y - 10, snake_unit_size, snake_unit_size);
+    ctx.fillRect(Player.state.x - 10, Player.state.y - 10, Player.size, Player.size);
 
     // Draw the yellow dots (snake food)
     ctx.fillStyle = "yellow";
@@ -110,8 +158,11 @@ var lastRender = 0;
 window.requestAnimationFrame(loop);
 
 
-// KEYBOARD INPUT CODE
 
+
+//==============================================
+// keyboard input listener
+//==============================================
 
 window.addEventListener("keydown", function (event) {
     if (event.defaultPrevented) {
@@ -121,16 +172,16 @@ window.addEventListener("keydown", function (event) {
     // Modify position with arrow keys
     switch (event.key) {
         case "ArrowDown":
-            direction = "down";
+            Player.direction = "down";
             break;
         case "ArrowUp":
-            direction = "up";
+            Player.direction = "up";
             break;
         case "ArrowLeft":
-            direction = "left";
+            Player.direction = "left";
             break;
         case "ArrowRight":
-            direction = "right";
+            Player.direction = "right";
             break;
     }
 }, true);
