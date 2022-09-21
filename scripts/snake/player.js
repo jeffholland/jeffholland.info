ad53a76c15107b897f97a3ff283aa2b993b585cf
Jeff Holland <jh6547@nyu.edu>
2022-09-20 23:52:17 -0400

    changed player speed

--- scripts/snake/player.js
+++ scripts/snake/player.js
@@ -11,19 +11,22 @@ const Player = {
         y: (height / 2)
     },
 
+    // lower speed is slower (1 is update speed which is very fast)
+    speed: 0.5,
+
     move: function(progress) {
             switch (this.direction) {
                 case "up":
-                    this.state.y -= progress;
+                    this.state.y -= progress * this.speed;
                     break;
                 case "down":
-                    this.state.y += progress;
+                    this.state.y += progress * this.speed;
                     break;
                 case "left":
-                    this.state.x -= progress;
+                    this.state.x -= progress * this.speed;
                     break;
                 case "right":
-                    this.state.x += progress;
+                    this.state.x += progress * this.speed;
                     break;
             }
 
