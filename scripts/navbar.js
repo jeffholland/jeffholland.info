ea0049d9dfc499dfed9e59636ef87b5b0db996f2
Jeff Holland <holland.musictech@gmail.com>
2023-02-02 09:34:03 -0500

    added navbar script. cleaned up contact page. made titles not in all caps

---
+++ scripts/navbar.js
@@ -0,0 +1,19 @@
+navbar = document.getElementById("navbar");
+navbar.innerHTML = `
+<table class="navbar" cellspacing="10">
+  <tr>
+    <td>
+      <a class="nav-link" href="index.html">HOME</a>
+    </td>
+    <td>
+      <a class="nav-link" href="projects.html">PROJECTS</a>
+    </td>
+    <td>
+      <a class="nav-link" href="music.html">MUSIC</a>
+    </td>
+    <td>
+      <a class="nav-link" href="contact.html">CONTACT</a>
+    </td>
+  </tr>
+</table>
+`
