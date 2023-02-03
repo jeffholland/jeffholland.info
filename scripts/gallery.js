edcb2db96ac04af6952abc028465aa0b2e526df7
Jeff Holland <holland.musictech@gmail.com>
2023-02-03 17:57:39 -0500

    expanded gallery of glitch art

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,13 +1,33 @@
 const table = document.getElementById("gallery");
 
+/* 
+    Filenames need to be added manually here,
+    because apparently you can't scan files in client-side JS.
+*/
 const files = [
     "cat",
+    "smile",
+    "cat2",
     "sbwy-1",
-    "sbwy-2",
     "sbwy-3",
-    "sbwy-4b",
     "sbwy-4c",
-    "sbwy-8"
+    "sbwy-8",
+    "e1",
+    "e2",
+    "rbj",
+    "c-c",
+    "c-f",
+    "etc1",
+    "etc2",
+    "me3",
+    "me4",
+    "me6",
+    "sunset1",
+    "sunset2",
+    "sunset3",
+    "sunset4",
+    "sunset5",
+    "sunset6",
 ];
 
 const tableWidth = 4;
@@ -19,28 +39,19 @@ for (let i = 0; i < files.length; i++) {
 
     // Start of row
     if (i % tableWidth == 0) {
-        htmlString += `
-        <tr>
-            <td>
-                <img src=\"images/gallery/${filename}.png\">
-            </td>
-        `
+        htmlString += "<tr>"
     }
+
+    htmlString += `
+        <td>
+            <a href=\"images/gallery/${filename}.png\" target="_blank">
+                <img src=\"images/gallery/${filename}.png\">
+            </a>
+        </td>
+    `
     // End of row
-    else if (i % tableWidth == tableWidth - 1) {
-        htmlString += `
-            <td>
-                <img src=\"images/gallery/${filename}.png\">
-            </td>
-        </tr>
-        `
-    }
-    // Middle of row
-    else {
-        htmlString += `
-            <td>
-                <img src=\"images/gallery/${filename}.png\">
-            </td>`
+    if (i % tableWidth == tableWidth - 1) {
+        htmlString += "</tr>"
     }
 }
 
